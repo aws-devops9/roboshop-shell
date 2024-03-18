d8d4d1179ec497a9059943188acfa9d0359e9ba0
cloudtechprem <cloudtechprem@gmail.com>
2024-03-18 13:21:16 +0530

    1st commit

---
+++ roboshop.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Algorithm
+# 
+#
+#
+#
+#
+#
+#
+#
+#
+
+AMI=ami-0f3c7d07486cad139
+SG_ID=sg-04d3cc3675c0c646f
+INSTANCES=("mongodb" "redis" "mysql" "catalogue" "user" "cart" "shipping" "payment" "rabbitmq" "dispatch" "web")
+
+for i in "${$INSTANCES[@]}"
+do
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+        INSTANCE_TYPE="t2.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+
+    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID
+
+done
